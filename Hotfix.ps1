c4ccb7df66e54568a1f45332263cf58ca045b070
Karanjit Oberoi <95193927+KaranjitOberoi@users.noreply.github.com>
2022-01-29 08:00:40 -0800

    Add files via upload

---
+++ Hotfix.ps1
@@ -0,0 +1,15 @@
+﻿$outputfolder = 'E:\Practice Powershell\hotfix.csv'
+function hf{
+[cmdletbinding()]
+Param (
+   $hotfixdmy = (Read-Host "Enter the numbnerof days":)
+      )
+      #$hotfixdmy
+      #$res= Get-HotFix | Where-Object -Property installedon -LT $hotfixdmy| Export-Csv -Path $outputfolder
+      $dayscal =((Get-Date).AddDays(- $hotfixdmy))
+       $dayscal
+      $res= Get-HotFix | Where-Object -Property installedon -gT $daycal
+      $res 
+ 
+}
+hf 
---
+++ Hotfix_updates.ps1
@@ -0,0 +1,14 @@
+﻿function hf{
+[cmdletbinding()]
+Param (
+   $hotfixdmy = (Read-Host "Filter systems that have hot fixes done less than the input date  dd/mm/yyyy format")
+      )
+      $conv =[datetime]::ParseExact($hotfixdmy,'dd/mm/yyyy',$null)
+      $conv
+      $res= Get-HotFix | Where-Object -Property installedon -LT $conv | ft
+      #$outputfolder = 'E:\Practice Powershell\hotfix.csv'
+      #Export-Csv -Path $outputfolder
+      #Write-Verbose $res
+        #$res
+}
+hf 
